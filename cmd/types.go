3b4aac454cd1f5c50145bd5915fcd68b9fd4f5e0
John Doe <zzgigi2003@163.com>
2018-10-04 13:19:09 +0800

    增加csr结构体

--- cmd/types.go
+++ cmd/types.go
@@ -1,5 +1,10 @@
-package main
+package cmd
 
-func main() {
-	$END$
+import "github.com/tendermint/tendermint/crypto/ed25519"
+
+type Csr struct {
+	Version   int8                  `json:"version"`
+	CA        bool                  `json:"ca"`
+	CN        string                `json:"cn"`
+	PublicKey ed25519.PubKeyEd25519 `json:"public_key"`
 }
