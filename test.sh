0592da843f4833ef2085144af1b0e99b36cea9a4
John Doe <zzgigi2003@163.com>
2018-10-08 14:49:17 +0800

    签发证书后，等待1秒

--- test.sh
+++ test.sh
@@ -18,21 +18,24 @@ $PWD/$CMD genkey
 $PWD/$CMD trust --in-public-key key.pub --out-trust-crts trust.crts
 $PWD/$CMD req
 $PWD/$CMD sign
+sleep 1
 $PWD/$CMD verify
-$PWD/$CMD show
+# $PWD/$CMD show
 
 
 # QOS 
 $PWD/$CMD genkey --out-private-key qos.pri --out-public-key qos.pub $VERBOSE
 $PWD/$CMD req --in-public-key qos.pub --cn QOS --out-sign-req qos.csr
 $PWD/$CMD sign  --in-key-pri key.pri --in-key-pub key.pub --in-sign-req qos.csr --out-signed-ca qos.crt
+sleep 1
 $PWD/$CMD verify --in-signed-ca qos.crt
-$PWD/$CMD show --in-csr-file qos.csr --in-crt-file qos.crt
+# $PWD/$CMD show --in-csr-file qos.csr --in-crt-file qos.crt
 
 
 # QSC
 $PWD/$CMD genkey --out-private-key qsc.pri --out-public-key qsc.pub $VERBOSE
 $PWD/$CMD req --in-public-key qsc.pub --cn QSC --out-sign-req qsc.csr
 $PWD/$CMD sign  --in-key-pri key.pri --in-key-pub key.pub --in-sign-req qsc.csr --out-signed-ca qsc.crt
+sleep 1
 $PWD/$CMD verify --in-signed-ca qsc.crt
-$PWD/$CMD show --in-csr-file qsc.csr --in-crt-file qsc.crt
+# $PWD/$CMD show --in-csr-file qsc.csr --in-crt-file qsc.crt
