3857f4e55b22ea3ebf8c89fb3f99397ff679c2a3
John Doe <zzgigi2003@163.com>
2018-11-01 15:41:34 +0800

    增加测试用例cert/types_test.go

---
+++ cert/types_test.go
@@ -0,0 +1,41 @@
+package cert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+)
+
+func TestGetQscName(t *testing.T) {
+	csr := CertificateSigningRequest{}
+	csr.Subj.CN = "QOSC1"
+
+	crt := Certificate{}
+	crt.CSR = csr
+
+	assert.Equal(t, "QOSC1", crt.GetQscName())
+}
+
+func TestIsBanker(t *testing.T) {
+	csr := CertificateSigningRequest{}
+	csr.IsBanker = true
+
+	crt := Certificate{}
+	crt.CSR = csr
+
+	assert.Equal(t, true, crt.IsBanker())
+}
+
+func TestGetPublicKey(t *testing.T) {
+	privKey := ed25519.GenPrivKey()
+	pubKey := privKey.PubKey()
+
+	csr := CertificateSigningRequest{}
+	csr.PublicKey = pubKey.(ed25519.PubKeyEd25519)
+
+	crt := Certificate{}
+	crt.CSR = csr
+
+	assert.Equal(t, true, pubKey.Equals(crt.GetPublicKey()))
+}
